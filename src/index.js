30b5bfbb24f0ab4aa78668f717184730a9083c3f
Anurag Pratik <anurag.pratik@yahoo.com>
2022-07-13 16:55:40 +0530

    redux-devtools-extension

--- src/index.js
+++ src/index.js
@@ -19,31 +19,66 @@
 // console.log("Learning redux");
 
 // level 2 - npm i redux-logger --save-dev
-import { applyMiddleware, legacy_createStore as createStore } from "redux";
-import logger from "redux-logger"; //appollo, thung, saga
+// import { applyMiddleware, legacy_createStore as createStore } from "redux";
+// import logger from "redux-logger"; //appollo, thung, saga
 
-const reducer = function (state, action) {
-  if (action.type === "INC") {
-    return state + action.payload;
-  } else if (action.type === "DEC") {
-    return state - action.payload;
-  } else if (action.type === "MUL") {
-    return state * action.payload;
-  } else {
-    return state;
+// const reducer = function (state, action) {
+//   if (action.type === "INC") {
+//     return state + action.payload;
+//   } else if (action.type === "DEC") {
+//     return state - action.payload;
+//   } else if (action.type === "MUL") {
+//     return state * action.payload;
+//   } else {
+//     return state;
+//   }
+// };
+
+// const middleware = applyMiddleware(logger);
+
+// const store = createStore(reducer, 1, middleware);
+
+// store.subscribe(() => {
+//   console.log(`Store changed - ${store.getState()}`); //165
+// });
+
+// store.dispatch({ type: "INC", payload: 5 });
+// store.dispatch({ type: "DEC", payload: 3 });
+// store.dispatch({ type: "INC", payload: 8 });
+// store.dispatch({ type: "MUL", payload: 15 });
+// store.dispatch({ type: "DIV", payload: 2 });
+
+// level 3 - combine reducers, dev tools - npm i redux-devtools-extension --save-dev
+import {
+  applyMiddleware,
+  combineReducers,
+  legacy_createStore as createStore,
+} from "redux";
+import logger from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const userReducer = (state = {}, action) => {
+  switch (action.type) {
+    case "CHANGE_NAME":
+      return (state = { ...state, name: action.payload }); //{user:{name:"Adam"}}
+    case "CHANGE_AGE":
+      return (state = { ...state, age: action.payload }); //{user:{name:"Adam", age:50}}
+    default:
+      return state;
   }
 };
-
-const middleware = applyMiddleware(logger);
-
-const store = createStore(reducer, 1, middleware);
-
-store.subscribe(() => {
-  console.log(`Store changed - ${store.getState()}`); //165
+const tweetsReducer = (state = [], action) => {
+  return state;
+};
+const allReducers = combineReducers({
+  user: userReducer,
+  tweets: tweetsReducer,
 });
+const store = createStore(
+  allReducers,
+  composeWithDevTools(applyMiddleware(logger))
+);
 
-store.dispatch({ type: "INC", payload: 5 });
-store.dispatch({ type: "DEC", payload: 3 });
-store.dispatch({ type: "INC", payload: 8 });
-store.dispatch({ type: "MUL", payload: 15 });
-store.dispatch({ type: "DIV", payload: 2 });
+store.dispatch({ type: "CHANGE_NAME", payload: "Adam" });
+store.dispatch({ type: "CHANGE_AGE", payload: 50 });
+store.dispatch({ type: "CHANGE_NAME", payload: "Brock" });
