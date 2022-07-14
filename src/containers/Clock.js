d7813465ccaf3dcda95648e120078da3a5043bea
Anurag Pratik <anurag.pratik@yahoo.com>
2022-07-14 15:38:19 +0530

    adds clock timer code

--- src/containers/Clock.js
+++ src/containers/Clock.js
@@ -1,10 +1,28 @@
 import React, { Component } from "react";
 
 export default class Clock extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+    this.timer = null;
+  }
+
+  updateTime() {
+    this.setState({ date: new Date() });
+  }
+
+  componentDidMount() {
+    this.timer = setInterval(() => this.updateTime(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
   render() {
     return (
       <div>
-        <h3>Now the time is : {new Date().toLocaleTimeString()}</h3>
+        <h3>Now the time is : {this.state.date.toLocaleTimeString()}</h3>
       </div>
     );
   }
