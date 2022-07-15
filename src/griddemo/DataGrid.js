984712c950c9d7978bd312df776645d49d0664ac
Anurag Pratik <anurag.pratik@yahoo.com>
2022-07-15 12:26:08 +0530

    rest datagrid to initial state

--- src/griddemo/DataGrid.js
+++ src/griddemo/DataGrid.js
@@ -1,65 +1,25 @@
-import React, {
-  useState,
-  useRef,
-  useEffect,
-  useMemo,
-  useCallback,
-} from "react";
-import { AgGridReact } from "ag-grid-react"; // the AG Grid React Component
+import React, { useState } from "react";
+import { AgGridReact } from "ag-grid-react";
 
-import "ag-grid-community/styles/ag-grid.css"; // Core grid CSS, always needed
-import "ag-grid-community/styles/ag-theme-alpine.css"; // Optional theme CSS
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
 
 const DataGrid = () => {
-  const gridRef = useRef(); // Optional - for accessing Grid's API
-  const [rowData, setRowData] = useState(); // Set rowData to Array of Objects, one Object per Row
+  const [rowData] = useState([
+    { make: "Toyota", model: "Celica", price: 35000 },
+    { make: "Ford", model: "Mondeo", price: 32000 },
+    { make: "Porsche", model: "Boxster", price: 72000 },
+  ]);
 
-  // Each Column Definition results in one Column.
-  const [columnDefs, setColumnDefs] = useState([
-    { field: "make", filter: true },
-    { field: "model", filter: true },
+  const [columnDefs] = useState([
+    { field: "make" },
+    { field: "model" },
     { field: "price" },
   ]);
 
-  // DefaultColDef sets props common to all Columns
-  const defaultColDef = useMemo(() => ({
-    sortable: true,
-  }));
-
-  // Example of consuming Grid Event
-  const cellClickedListener = useCallback((event) => {
-    console.log("cellClicked", event);
-  }, []);
-
-  // Example load data from sever
-  useEffect(() => {
-    fetch("https://www.ag-grid.com/example-assets/row-data.json")
-      .then((result) => result.json())
-      .then((rowData) => setRowData(rowData));
-  }, []);
-
-  // Example using Grid's API
-  const buttonListener = useCallback((e) => {
-    gridRef.current.api.deselectAll();
-  }, []);
-
   return (
-    <div>
-      {/* Example using Grid's API */}
-      <button onClick={buttonListener}>Push Me</button>
-
-      {/* On div wrapping Grid a) specify theme CSS Class Class and b) sets Grid size */}
-      <div className="ag-theme-alpine" style={{ width: 500, height: 500 }}>
-        <AgGridReact
-          ref={gridRef} // Ref for accessing Grid's API
-          rowData={rowData} // Row Data for Rows
-          columnDefs={columnDefs} // Column Defs for Columns
-          defaultColDef={defaultColDef} // Default Column Properties
-          animateRows={true} // Optional - set to 'true' to have rows animate when sorted
-          rowSelection="multiple" // Options - allows click selection of rows
-          onCellClicked={cellClickedListener} // Optional - registering for Grid Event
-        />
-      </div>
+    <div className="ag-theme-alpine" style={{ height: 400, width: 600 }}>
+      <AgGridReact rowData={rowData} columnDefs={columnDefs}></AgGridReact>
     </div>
   );
 };
