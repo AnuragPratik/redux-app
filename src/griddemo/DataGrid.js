286baae332429993696d07672fd53ca8f082d13e
Anurag Pratik <anurag.pratik@yahoo.com>
2022-07-15 12:33:51 +0530

    pulls users data and shows in grid

--- src/griddemo/DataGrid.js
+++ src/griddemo/DataGrid.js
@@ -5,20 +5,39 @@ import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
 
 const DataGrid = () => {
-  const [rowData] = useState([
-    { make: "Toyota", model: "Celica", price: 35000 },
-    { make: "Ford", model: "Mondeo", price: 32000 },
-    { make: "Porsche", model: "Boxster", price: 72000 },
-  ]);
+  const [rowData, setRowData] = useState([]);
+  let columnDefs = null;
+  columnDefs = [
+    {
+      headerName: "ID",
+      field: "id",
+      sortable: true,
+      filter: true,
+      checkboxSelection: true,
+    },
+    {
+      headerName: "User Name",
+      field: "username",
+      sortable: true,
+      filter: true,
+    },
+    {
+      headerName: "Email",
+      field: "email",
+      sortable: true,
+      filter: true,
+    },
+  ];
 
-  const [columnDefs] = useState([
-    { field: "make" },
-    { field: "model" },
-    { field: "price" },
-  ]);
+  const handleClick = () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((resp) => resp.json())
+      .then((data) => setRowData(data));
+  };
 
   return (
     <div className="ag-theme-alpine" style={{ height: 500, width: 650 }}>
+      <button onClick={handleClick}>Show Users</button>
       <AgGridReact rowData={rowData} columnDefs={columnDefs}></AgGridReact>
     </div>
   );
